35084166a37bf77e42affe8370933c6e63df6193
vikas-mangla <vikas.mangla@salesforce.com>
2020-05-18 23:17:15 +0530

    Added

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -1,23 +1,9 @@
-/*
- * Copyright 2002-2014 the original author or authors.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 package com.example;
 
 import com.zaxxer.hikari.HikariConfig;
 import com.zaxxer.hikari.HikariDataSource;
+import org.jscience.physics.amount.Amount;
+import org.jscience.physics.model.RelativisticModel;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
@@ -26,6 +12,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import javax.measure.quantity.Mass;
 import javax.sql.DataSource;
 import java.sql.Connection;
 import java.sql.ResultSet;
@@ -33,6 +20,8 @@ import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.Map;
+
+import static javax.measure.unit.SI.KILOGRAM;
 
 @Controller
 @SpringBootApplication
@@ -53,9 +42,35 @@ public class Main {
     return "index";
   }
 
+  @RequestMapping("/hello")
+  String hello(Map<String, Object> model) {
+    RelativisticModel.select();
+    Amount<Mass> m = Amount.valueOf("12 GeV").to(KILOGRAM);
+    model.put("science", "E=mc^2: 12 GeV = " + m.toString());
+    return "hello";
+  }
+
   @RequestMapping("/db")
   String db(Map<String, Object> model) {
+    try (Connection connection = dataSource.getConnection()) {
+      Statement stmt = connection.createStatement();
+      ResultSet rs = stmt.executeQuery("SELECT Firstname, Lastname, Email FROM salesforce.contact limit 10");
+
+      List<List<String>> outputList = new ArrayList<List<String>>();
+      while (rs.next()) {
+      	List<String> output = new ArrayList<String>();
+        output.add(rs.getString("Firstname"));
+        output.add(rs.getString("Lastname"));
+        output.add(rs.getString("Email"));
+        outputList.add(output);
+      }
+
+      model.put("records", output);
       return "db";
+    } catch (Exception e) {
+      model.put("message", e.getMessage());
+      return "error";
+    }
   }
 
   @Bean
